c72c7fc50e457811cb7b6361b356ccee8dc1eb46
Rafael Magana <rafialmadrid@gmail.com>
2021-01-13 22:03:42 -0600

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chilaquiles Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tamal Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
